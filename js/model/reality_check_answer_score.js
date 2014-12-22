7d19234ca0fd956613ebf3807aa1513cbe36f339
lykong <konglingy_lady@sina.com>
2014-12-23 00:40:29 +0800

    Rewrite getScore() in RealityCheckAnswerScore

--- js/model/reality_check_answer_score.js
+++ js/model/reality_check_answer_score.js
@@ -7,10 +7,6 @@ RealityCheckAnswerScore.prototype = Object.create(RealityAnswer.prototype);
 RealityCheckAnswerScore.prototype.constructor = RealityCheckAnswerScore;
 
 RealityCheckAnswerScore.prototype.getScore = function(elementName) {
-  var score = 0;
-
-  var findAllAnswer = DefaultAnswer.all();
-
   var _this = this;
 
   var answer = [];
@@ -20,12 +16,8 @@ RealityCheckAnswerScore.prototype.getScore = function(elementName) {
       answer.push(oneNum.value);
     }
 
-    for(var i = 0; i < findAllAnswer.length; i++) {
-      if(oneNum.name === findAllAnswer[i].questionNum) {
-        score = (answer.toString() === findAllAnswer[i].defaultAnswer.toString()) ? findAllAnswer[i].score : 0;
-      }
+    if(oneNum.name === findAllAnswer[i].questionNum) {
+      return (answer.toString() === elementName.value.toString()) ? this.score : 0;
     }
   });
-
-  return score;
 };
