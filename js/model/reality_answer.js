f058d32d3d583dc50956cfbab3e3a2a59a66aa0d
lykong <konglingy_lady@sina.com>
2014-12-22 19:32:09 +0800

    Add new Father Class RealityAnswer.

---
+++ js/model/reality_answer.js
@@ -0,0 +1,9 @@
+function RealityAnswer(questionNum, defaultAnswer, score) {
+  this.questionNum = questionNum;
+  this.defaultAnswer = defaultAnswer;
+  this.score = score;
+}
+
+RealityAnswer.prototype.getScore = function() {
+
+};
