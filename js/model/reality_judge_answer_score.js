d0499fa40635a2a9e3138698b03ebe8571bab3f4
lykong <konglingy_lady@sina.com>
2014-12-23 00:54:13 +0800

    Rewrite getScore() in RealityJudgeAnswerScore.

--- js/model/reality_judge_answer_score.js
+++ js/model/reality_judge_answer_score.js
@@ -7,21 +7,11 @@ RealityJudgeAnswerScore.prototype = Object.create(RealityAnswer.prototype);
 RealityJudgeAnswerScore.prototype.constructor = RealityJudgeAnswerScore;
 
 RealityJudgeAnswerScore.prototype.getScore = function(elementName) {
-  var score = 0;
-
-  var findAllAnswer = DefaultAnswer.all();
-
   var _this = this;
 
-  var oneNum = _.find(_this.questionNum, function(oneNum) {
-    return oneNum;
-  });
+  var oneNum = _.find(elementName, { checked : true });
 
-  for(var i = 0; i < findAllAnswer.length; i++) {
-    if(oneNum.name === findAllAnswer[i].questionNum) {
-      score = (this.realityAnswers === findAllAnswer[i].defaultAnswer.toString()) ? findAllAnswer[i].score : 0;
-    }
+  if(oneNum) {
+    return this.realityAnswer === oneNum.value ? this.score : 0;
   }
-
-  return score;
 };
