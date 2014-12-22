3c8be5bee197134d26c58da856f3e7c261e53fe2
lykong <konglingy_lady@sina.com>
2014-12-23 01:03:50 +0800

    Add a parameter totalScore in RealityAnswer.

--- js/model/reality_answer.js
+++ js/model/reality_answer.js
@@ -2,6 +2,7 @@ function RealityAnswer(questionNum, realityAnswer, score) {
   this.questionNum = questionNum;
   this.realityAnswer = realityAnswer;
   this.score = score;
+  this.totalScore = totalScore;
 }
 
 RealityAnswer.prototype.getScore = function(elementName) {
